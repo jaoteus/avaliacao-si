01a89241a4b2639afe4bf8c869472d4db78cedf1
Mateus <mateusff2323@gmail.com>
2023-11-27 10:58:39 -0300

    atualizacao

--- Main.py
+++ Main.py
@@ -1,4 +1,75 @@
-'''
-REGRAS: não
+class App:
+    def __init__(self):
+        self.pontuacao = 0
+        self.regras()
 
-'''
+    def regras(self):
+        print(f'')
+
+
+    def pergunta_1(self):
+        self.pergunta_2()
+        pass
+    def pergunta_2(self):
+        self.pergunta_3()
+        pass
+    def pergunta_3(self):
+        self.pergunta_4()
+        pass
+    def pergunta_4(self):
+        self.pergunta_5()
+        pass
+    def pergunta_5(self):
+        self.pergunta_6()
+        pass
+    def pergunta_6(self):
+        self.pergunta_7()
+        pass
+    def pergunta_7(self):
+        self.pergunta_8()
+        pass
+    def pergunta_8(self):
+        self.pergunta_9()
+        pass
+    def pergunta_9(self):
+        self.pergunta_10()
+        pass
+    def pergunta_10(self):
+        self.pergunta_11()
+        pass
+    def pergunta_11(self):
+        self.pergunta_12()
+        pass
+    def pergunta_12(self):
+        self.pergunta_13()
+        pass
+    def pergunta_13(self):
+        self.pergunta_14()
+        pass
+    def pergunta_14(self):
+        self.pergunta_15()
+        pass
+    def pergunta_15(self):
+        self.pergunta_16()
+        pass
+    def pergunta_16(self):
+        self.pergunta_17()
+        pass
+    def pergunta_17(self):
+        self.pergunta_18()
+        pass
+    def pergunta_18(self):
+        self.pergunta_19()
+        pass
+    def pergunta_19(self):
+        self.pergunta_20()
+        pass
+    def pergunta_20(self):
+        self.mostrar_resultado()
+        pass
+    def mostrar_resultado(self):
+        pass
+
+
+if __name__ == '__main__':
+    App()
